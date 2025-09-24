dcd0e73c834a05b947cbd1beb08e7c8ecfe9101f
lucas <joaolucas1444@gmail.com>
2025-09-24 11:22:38 -0300

    1 ano depois

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  distDir: 'dist',
-  basePath: isProd ?  '/My_n_portfolio' : ''
+  /* se quiser, pode deixar vazio mesmo */
 };
 
 export default nextConfig;
