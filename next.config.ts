018a39912904918028afb2b86a2bcaa0fd148934
lucas <joaolucas1444@gmail.com>
2025-09-24 10:56:28 -0300

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'dist',
+  basePath: isProd ?  'My_n_portfolio' : ''
 };
 
 export default nextConfig;
