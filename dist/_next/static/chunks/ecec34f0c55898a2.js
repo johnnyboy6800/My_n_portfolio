1067d8c0b21f95b72c542391888c3cb28636fcbb
lucas <joaolucas1444@gmail.com>
2025-09-24 10:19:55 -0300

    static files added

---
+++ dist/_next/static/chunks/ecec34f0c55898a2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/e2322fdb40af2e42.js",
+  "static/chunks/turbopack-53d73c8b246e18fc.js"
+])
--- src/app/NavBar/page.tsx
+++ src/app/NavBar/page.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { Interface } from "readline";
+'use client'
 
 
 interface NavBarProps {
