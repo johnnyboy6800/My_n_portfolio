c4f088967dde01fee7bd390b84736b70d2bc788a
lucas <joaolucas1444@gmail.com>
2025-09-24 11:19:03 -0300

    ainda tentando corrigir isso

--- src/app/especialidades/page.tsx
+++ src/app/especialidades/page.tsx
@@ -7,15 +7,15 @@ export default function Especialidades() {
             <div id="skills">
                 
                 <ul id="skills-list1">
-                    <img id="icons" src="${basePath}/server.png"></img>
+                    <img id="icons" src="/server.png"></img>
                     <li id="skill-list"><h3 id="skill-title">Lógica de servidores</h3> <br/> Criação de regras e configurações que definem como os servidores se comportam</li> 
                 </ul>
                  <ul id="skills-list1">
-                    <img id="icons" src="${basePath}/apis.png"></img>
+                    <img id="icons" src="/apis.png"></img>
                     <li id="skill-list"><h3 id="skill-title">Desenvolvimento de API'S</h3> <br/> Criação de API'S robustas com foco em perfomance e eficiência</li> 
                 </ul>
                 <ul id="skills-list1">
-                    <img id="icons" src="${basePath}/images.png"></img>
+                    <img id="icons" src="/images.png"></img>
                     <li id="skill-list"><h3 id="skill-title">Gerenciamento de banco de dados </h3> <br/> Criação de bancos de dados e consultas complexas.</li> 
                 </ul>
             </div>
